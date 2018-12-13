308c0dce2f6bf8524c2b32d624e06700d2905a32
GregJones <g.jones18@imperial.ac.uk>
2018-12-13 16:07:28 +0000

    test file updated to reflect current sparse matrix structure

--- vectest.f90
+++ vectest.f90
@@ -1,16 +1,42 @@
 program vectest
-  use NLengthVector
+  !!-------------------------------------------------------------------------!!
+  !!-------------------------------------------------------------------------!!
+  !! Author: Greg Jones ------------------------------------ Date: 14/12/18 -!!
+  !! Intent: A test program for the compressed row storage class and --------!!
+  !!         corresponding bespoke matrix operations ------------------------!!
+  !! Requirements: SparseMatrixForm.f90 and contained classes ---------------!!
+  !!-------------------------------------------------------------------------!!
+  !! Inputs: Row and column data --------------------------------------------!!
+  !! Outputs: A compressed row storage matrix capable of performing matrix --!!
+  !!          operations ----------------------------------------------------!!
+  !! Revisions: -------------------------------------------------------------!!
+  !! 1st (14/12/18) - Development of a general test program showing the -----!!
+  !!                  compressed storage capability and 2x2 matrix ----------!!
+  !!                  multiplication ----------------------------------------!!
+  !!-------------------------------------------------------------------------!!
+  !!-------------------------------------------------------------------------!!
+  use SparseMatrixForm
   implicit none
 
-  type (Vector) :: vec
+  type (sparseMatrix) :: sm
   double precision, dimension(10) :: vals
+  integer, dimension(10)            ::    col_in
   integer :: i
   double precision :: add = 5
   integer :: index = 11
+  integer                  ::    rows
 
+  !! Test File Inputs !!
+  write(*,*) "Enter Num rows: "
+  read(*,*) rows
+
+  col_in = (/ (i,i=1,10) /)
   vals = (/ (i,i=1,10) /)
 
-  vec = Vector(vals)
-  call vec%addElement(add,index)
-  write(*,*) vec%vals, size(vec%vals), vec%populated
+  sm = sparseMatrix(5)
+  do i=1, rows
+    call sm%addRowVals(i, col_in, vals)
+  enddo
+  write(*,*) sm%getValue(1,1)
+  !sm%destroySM()
 end program
